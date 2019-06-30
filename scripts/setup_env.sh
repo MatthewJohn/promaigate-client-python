2c8423f535084fb24378ba61503ba77d6992b37e
Matt Comben <matthew@dockstudios.co.uk>
2019-06-30 15:57:04 +0100

    Update to inject pip.conf in setup_env

--- scripts/setup_env.sh
+++ scripts/setup_env.sh
@@ -11,3 +11,13 @@ pip install "devpi-client>=2.3.0"
 pip download -r requirements.txt -d build-env/reqs
 pip install -r requirements.txt
 
+cat > build-env/.pip.conf <<'EOF'
+[global]
+timeout = 60
+extra-index-url = http://fare-docker-reg.dock.studios:3141/dockstudios
+
+[install]
+trusted-host = fare-docker-reg.dock.studios
+EOF
+
+
