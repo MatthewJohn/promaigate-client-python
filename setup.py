f6750d0afe8fe2bcb05039dfe5c8c8bf4fccf1c3
Matt Comben <matthew@dockstudios.co.uk>
2019-06-30 14:26:46 +0100

    Update version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup
 
 setup(
     name='promailgate-client',
-    version='0.0.1',
+    version='0.0.2',
     packages=['promailgate_client'],
     url='https://phabricator.dockstudios.co.uk/',
     license='',
