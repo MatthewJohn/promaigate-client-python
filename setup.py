669e18ce6d79f345d428fcf567b93a9ef85d9823
Matt Comben <matthew@dockstudios.co.uk>
2019-06-30 10:24:12 +0100

    Add package

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='promailgate-client',
+    version='0.0.1',
+    packages=['promailgate_client'],
+    url='https://phabricator.dockstudios.co.uk/',
+    license='',
+    author='Matt Comben',
+    author_email='matthew@dockstudios.co.uk',
+    description='Python send client for promailgate'
+)
