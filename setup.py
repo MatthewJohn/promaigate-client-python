c4f34b347903927d7de1c17a3dd570e6174bbe47
Matt Comben <matt.comben@jobsite.co.uk>
2019-07-02 07:50:58 +0100

    Update to requests due to vulernability in version used

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup
 
 setup(
     name='promailgate-client',
-    version='1.0.1',
+    version='1.0.2',
     packages=['promailgate_client'],
     url='https://phabricator.dockstudios.co.uk/',
     license='',
